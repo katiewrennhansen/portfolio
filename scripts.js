31b4c321fe6909d004a8aea0e99900d41fcad0ae
Katie Hansen <48529509+katiewrennhansen@users.noreply.github.com>
2019-04-13 10:58:56 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var button = document.querySelector('button');
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+button.addEventListener('click', function(){
+  document.getElementById("dropdown").classList.toggle("show");
+})
