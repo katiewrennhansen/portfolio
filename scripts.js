ce533f4f15451807d3c7fa244bfbb40b274f0d6c
Katie Hansen <kawrenn@Katies-MacBook-Pro.local>
2019-05-12 09:53:40 -0400

    connect vscode

--- scripts.js
+++
@@ -1,7 +0,0 @@
-var button = document.querySelector('button');
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-button.addEventListener('click', function(){
-  document.getElementById("dropdown").classList.toggle("show");
-})
