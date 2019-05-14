fc35b664dcae98daff256a1c0c97bf59c7b5bcdd
Katie Hansen <kawrenn@Katies-MacBook-Pro.local>
2019-05-14 19:00:32 -0400

    add responsive nav and dropdown

--- scripts.js
+++ scripts.js
@@ -1,31 +1,7 @@
-// var button = document.querySelector('button');
-//
-// /* When the user clicks on the button,
-// toggle between hiding and showing the dropdown content */
-// button.addEventListener('click', function(){
-//   document.getElementById("dropdown").classList.toggle("show");
-// });
+var button = document.querySelector('button');
 
-
-
-// var hover = document.querySelectorAll('.hover');
-// var overlay = document.querySelectorAll('.overlay');
-//
-// for(var i =0; i<hover.length; i++){
-//   hover[i].addEventListener("mouseover",
-//   function(){
-//   overlay.style.display = "inline-block";
-// });
-//   hover[i].addEventListener("mouseout",
-//   function(){
-//   overlay.style.display = "none";
-// });
-// }
-
-
-// document.getElementsByClassName('hover').onmouseover = function(){
-//     document.getElementsByClassName('overlay').style.display = 'inline-block';
-// }
-// document.getElementsByClassName('hover').onmouseout = function(){
-//     document.getElementsByClassName('overlay').style.display = 'none';
-// }
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+button.addEventListener('click', function(){
+  document.getElementById("dropdown").classList.toggle("show");
+});
