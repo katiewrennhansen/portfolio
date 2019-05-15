2963e277e2add3447b95d108a9f271ac5ac5c870
Katie Hansen <kawrenn@Katies-MacBook-Pro.local>
2019-05-15 19:52:51 -0400

    work on navbar

--- scripts.js
+++ scripts.js
@@ -3,5 +3,18 @@ var button = document.querySelector('button');
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 button.addEventListener('click', function(){
-  document.getElementById("dropdown").classList.toggle("show");
+  document.getElementById("navbar").classList.toggle("show");
+});
+
+
+button.addEventListener('click', function(){
+  document.getElementById('abt').classList.toggle("fade");
+});
+
+button.addEventListener('click', function(){
+  document.getElementById('work').classList.toggle("fade");
+});
+
+button.addEventListener('click', function(){
+  document.getElementById('contact').classList.toggle("fade");
 });
